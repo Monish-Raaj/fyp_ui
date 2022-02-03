93364214880d614caad409a41ceb5eedc8e6f877
Monish Raaj L <ml2@atlassian.com>
2022-02-03 23:35:05 +0530

    data displayed

--- src/App.vue
+++ src/App.vue
@@ -5,72 +5,165 @@
     </v-app-bar>
 
     <v-main>
-      <v-card
-        elevation="2"
-        light
-        outlined
-        shaped
-        max-width="1000"
-        class="pa-10 ma-10"
-      >
-        <v-card-title>
-          Amazon Review Analyser
-          <v-rating
-            :value="5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="20"
-          ></v-rating
-        ></v-card-title>
+      <v-row>
+        <v-spacer></v-spacer>
+        <div v-if="!responseData">
+          <v-card
+            elevation="2"
+            light
+            outlined
+            shaped
+            max-width="1000"
+            min-width="1000"
+            class="pa-10 ma-10"
+          >
+            <v-card-title>
+              Amazon Review Analyzer
+              <v-rating
+                :value="5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="20"
+                class="ml-2"
+              ></v-rating
+            ></v-card-title>
 
-        <v-card-text>
-          <v-row align="center" class="mx-0">
-            <v-text-field
-              label="Amazon Review URL"
-              :rules="rules"
-              hide-details="auto"
-              v-model="url"
-            ></v-text-field>
-            <v-btn elevation="2" large raised color="purple" @click="submit()" class="ml-5"
-            :loading="loading" dark
-              >Analyse
-              <v-icon dark> mdi-arrow-right </v-icon>
-            </v-btn>
-          </v-row>
-        </v-card-text>
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-text-field
+                  label="Amazon Review URL"
+                  :rules="rules"
+                  hide-details="auto"
+                  v-model="url"
+                ></v-text-field>
+                <v-btn
+                  elevation="2"
+                  large
+                  raised
+                  color="purple"
+                  @click="submit()"
+                  class="ml-5"
+                  :loading="loading"
+                  dark
+                  >Analyze
+                  <v-icon dark> mdi-arrow-right </v-icon>
+                </v-btn>
+              </v-row>
+            </v-card-text>
 
-        <v-divider class="mx-4"></v-divider>
-      </v-card>
+            <v-divider class="mx-4"></v-divider>
+          </v-card>
+        </div>
+        <div v-else>
+          <v-card
+            elevation="2"
+            light
+            outlined
+            shaped
+            max-width="1200"
+            min-width="1200"
+            class="pa-10 ma-10"
+          >
+            <v-card-title>
+              <h1 class="text-center" style="width: 100%">Review Analysis</h1>
+            </v-card-title>
+            <v-divider color="red"></v-divider>
+            <div class="small">
+              <pie-chart
+                :data="chartData1"
+                :options="chartOptions"
+                width="100px"
+                height="100px"
+              ></pie-chart>
+            </div>
+            <div class="small">
+              <pie-chart
+                :data="chartData2"
+                :options="chartOptions"
+                width="100px"
+                height="100px"
+              ></pie-chart>
+            </div>
+            <v-expansion-panels multiple v-model="panel">
+              <v-expansion-panel>
+                <v-expansion-panel-header>
+                  <h1>Positive Review Summary</h1>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  {{ responseData.PosSummary }}
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+              <v-expansion-panel>
+                <v-expansion-panel-header>
+                  <h1>Negative Review Summary</h1>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  {{ responseData.NegSummary }}
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+              <v-expansion-panel>
+                <v-expansion-panel-header>
+                  <h1>Feature Based Summary</h1>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-chip
+                    class="ma-2"
+                    v-for="(feature,index) in Object.keys(responseData.featureSummary)"
+                    :key="index"
+                  >
+                  {{feature}}
+                  </v-chip>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card>
+        </div>
+        <v-spacer></v-spacer>
+      </v-row>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
+import PieChart from "./PieChart.js";
+
 export default {
   name: "App",
 
-  components: {},
+  components: {
+    PieChart,
+  },
 
   data: () => ({
     url: null,
     rules: [(value) => !!value || "Required."],
-    loading: false
+    loading: false,
+    responseData: null,
+    chartOptions: {
+      responsive: true,
+      legend: {
+        display: true,
+      },
+    },
+    chartData1: require("./chart1Init.json"),
+    chartData2: require("./chart1Init.json"),
+    panel: [0,1,2],
   }),
-  methods:{
-    submit:async ()=>{
+  methods: {
+    async submit() {
       try {
-        this.loading=true
-        let res=await this.$http.get(`/db?url=${this.url}`)
-        console.log(res.data);
+        this.loading = true;
+        let res = require("./res.json");
+        this.responseData = res;
       } catch (error) {
-        alert(error)
+        alert(error);
       }
-      this.loading=false
-    }
-  }
+      this.loading = false;
+    },
+  },
 };
 </script>
 
@@ -79,4 +172,9 @@ export default {
   background: url(./assets/bg.jpeg) no-repeat center center fixed !important;
   background-size: cover;
 } */
+
+.small {
+  max-width: 400px;
+  margin: 10px;
+}
 </style>
